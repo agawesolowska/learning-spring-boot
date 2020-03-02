e4c2edd901a8e0033828c9d557ad5777d5fb3ab3
Aga <aga@aga.com>
2020-03-02 17:22:55 +0100

    added tests

--- src/main/java/pl/agawesolowska/learningspringboot/entity/Student.java
+++ src/main/java/pl/agawesolowska/learningspringboot/entity/Student.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,6 +23,7 @@ import lombok.Setter;
 @Table(name="student")
 @NoArgsConstructor
 @Getter @Setter
+@EqualsAndHashCode
 public class Student {
 
 	@Id
--- src/test/java/pl/agawesolowska/learningspringboot/dao/MockStudentDataAccessTests.java
+++ src/test/java/pl/agawesolowska/learningspringboot/dao/MockStudentDataAccessTests.java
@@ -1,13 +1,64 @@
 package pl.agawesolowska.learningspringboot.dao;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import pl.agawesolowska.learningspringboot.entity.Student;
+
 public class MockStudentDataAccessTests {
 
+	private MockStudentDataAccess underTest;
+
+	@Before
+	public void setUp() {
+		underTest = new MockStudentDataAccess();
+	}
+
 	@Test
-	void contextLoads() {
+	public void canPerformCrud() {
+
+		// Taking into consideration 3 students (by name, age and field of study):
+		UUID firstId = UUID.randomUUID();
+		Student firstStudent = new Student(firstId, "Fae Izsak", 23, "Chemistry");
+
+		UUID secondId = UUID.randomUUID();
+		Student secondStudent = new Student(secondId, "Fionnula Egdale", 20, "Computer Science");
+
+		UUID thirdId = UUID.randomUUID();
+		Student thirdStudent = new Student(thirdId, "Reagan Guppey", 24, "Medicine");
+
+		// Testing insertStudent method:
+		int firstExpectedResult = underTest.insertStudent(firstStudent);
+		int secondExpectedResult = underTest.insertStudent(secondStudent);
+		int thirdExpectedResult = underTest.insertStudent(thirdStudent);
+
+		assertEquals(1, firstExpectedResult);
+		assertEquals(1, secondExpectedResult);
+		assertEquals(1, thirdExpectedResult);
+
+		// Testing sellectAllStudents method:
+		List<Student> studentsList = underTest.selectAllStudents();
+		assertThat(studentsList).hasSize(3);
+
+		// Testing selectStudentById method:
+		Optional<Student> optionalStudent = underTest.selectStudentById(firstId);
+		assertEquals(firstStudent, optionalStudent);
+
+		// Testing updateStudentById method:
+		Student studentToUpdate = new Student(firstId, "Fae John Izsak", 25, "Biotechnology");
+		int fourthExpectedResult = underTest.updateStudentById(firstId, studentToUpdate);
+		assertEquals(1, fourthExpectedResult);
+
+		// Testing deleteStudentById method:
+		assertThat(underTest.deleteStudentById(firstId)).isEqualTo(1);
+
 	}
 
 }
